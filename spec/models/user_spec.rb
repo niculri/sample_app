8133e3ced2e7e0627827b53655b63fb360dcba53
Nicolas Ulriksen <piyomo@gmail.com>
2012-07-22 03:14:25 -0400

    finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -26,6 +26,7 @@ describe User do
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:remember_token) }
 
 	it { should be_valid }
 
@@ -109,6 +110,12 @@ describe User do
 	describe "with a password that's too short" do
 		before { @user.password = @user.password_confirmation = "a" * 5 }
 		it { should be_invalid }
+	end
+
+	describe "remember token" do
+		before { @user.save }
+		its(:remember_token) { should_not be_blank }
+		
 	end
 	
 end	
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -42,6 +42,11 @@ describe "User pages" do
 			it "should create a user" do
 				expect { click_button submit }.to change(User, :count).by(1)
 			end
+
+			describe "after saving the user" do
+				before { click_button submit }
+				it { should have_link('Sign out') }
+			end
 		end
 	end
 end
